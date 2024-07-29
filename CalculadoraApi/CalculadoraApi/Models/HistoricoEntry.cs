a28c666b62321da2088c59983071177d8970d2cb
Robson Ferreira Miranda <robsonferreiramiranda@Robsons-MacBook-Air.local>
2024-07-29 16:01:54 -0300

    feat: adicao historico de execucao

---
+++ CalculadoraApi/CalculadoraApi/Models/HistoricoEntry.cs
@@ -0,0 +1,12 @@
+﻿namespace CalculatorApi.Models
+{
+    public class HistoricoEntry
+    {
+        public int Id { get; set; }
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public string? Operation { get; set; }
+        public double Result { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
